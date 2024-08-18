e07911e66a221329f9d1bbe17f06681f2d13efb9
Aci <syafiqkunn023@gmail.com>
2024-08-19 00:01:19 +0700

    WORK IN PROGRESS

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 var speed
-const WALK_SPEED = 5.0
-const SPRINT_SPEED = 8.0
+const WALK_SPEED = 8.0
+const SPRINT_SPEED = 12.0
 const JUMP_VELOCITY = 4.8
 const SENSITIVITY = 0.002
 
